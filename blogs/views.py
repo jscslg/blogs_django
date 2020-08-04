f05b11a28e7702f4caff89f9368385b59c435c69
jscslg <jscslg27@gmail.com>
2020-08-04 21:43:00 +0530

    update

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView,DetailView
 
 def home_view(request):
     context= {
         'posts' : Post.objects.all()
     }
     return render(request,'blogs/home.html',context)
+
+class PostListView(ListView):
+    model=Post
+    template_name='blogs/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+class PostDetailView(DetailView):
+    model=Post
+    template_name='blogs/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
 
 def about_view(request):
     return render(request,'blogs/about.html',{})
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import *
+from .forms import UserUpdateForm,UserRegisterForm,ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -17,8 +17,16 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+    if(request.method=='POST'):
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if(u_form.is_valid and p_form.is_valid):
+            u_form.save()
+            p_form.save()
+            messages.success(request,f"Profile Updated Successfully")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
         'u_form':u_form,
         'p_form':p_form
