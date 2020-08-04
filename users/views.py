36d4775761b1821270dee27dc0419445ec01efa7
jscslg <jscslg27@gmail.com>
2020-08-04 12:06:44 +0530

    update

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import *
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -17,4 +17,10 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,"users/profile.html")
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,"users/profile.html",context)
